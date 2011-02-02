5591ba58a7ff5abb3b26193d40767dedfc6fea8d
Petter Holmström <petter.holmstrom@abo.fi>
2011-02-02 21:50:12 +0200

    Updated JavaDocs

--- Sources/MVP4Vaadin/src/com/github/peholmst/mvp4vaadin/navigation/Direction.java
+++ Sources/MVP4Vaadin/src/com/github/peholmst/mvp4vaadin/navigation/Direction.java
@@ -1,12 +1,27 @@
+/*
+ * Copyright (c) 2011 Petter Holmström
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.peholmst.mvp4vaadin.navigation;
 
 /**
- * TODO rewrite this documentation
+ * This enumeration defines the directions of the navigation inside
+ * a view controller stack.
  * 
- * This enumeration defines the directions of the current view change of a
- * view controller. If the change results in the stack growing, the
- * direction is <code>FORWARD</code>. Otherwise, the direction is
- * <code>BACKWARD</code>.
+ * @see ViewController
+ * @see ViewControllerListener
+ * @see ControllableView
  * 
  * @author Petter Holmström
  * @since 1.0

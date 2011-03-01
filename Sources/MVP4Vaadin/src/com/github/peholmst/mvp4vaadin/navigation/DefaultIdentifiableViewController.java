f3312e24875fc8362fd0fa264929e762d32642e9
Petter Holmström <petter.holmstrom@abo.fi>
2011-03-01 16:31:58 +0200

    Added DefaultIdentifiableViewController class

---
+++ Sources/MVP4Vaadin/src/com/github/peholmst/mvp4vaadin/navigation/DefaultIdentifiableViewController.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2011 Petter Holmström
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.peholmst.mvp4vaadin.navigation;
+
+/**
+ * This is the default implementation of the {@link IdentifiableViewController}
+ * interface. It does not require any special initialization, just create a new
+ * instance and start using it.
+ * 
+ * @author Petter Holmström
+ * @since 1.0
+ * @param <V>
+ *            the super interface of all views handled by this controller.
+ */
+public class DefaultIdentifiableViewController<V extends IdentifiableControllableView>
+		extends DefaultViewController<V> implements
+		IdentifiableViewController<V> {
+
+	private static final long serialVersionUID = -6566446894265364755L;
+
+	// TODO Test me!
+	
+	@Override
+	public boolean containsIdentifiableView(String viewIdentifier) {
+		if (viewStack.isEmpty()) {
+			return false;
+		}
+		for (V view : viewStack) {
+			if (view.getViewIdentifier().equals(viewIdentifier)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean goToIdentifiableView(String viewIdentifier) {
+		if (viewStack.isEmpty()) {
+			return false;
+		}
+		for (V view : viewStack) {
+			if (view.getViewIdentifier().endsWith(viewIdentifier)) {
+				return goToView(view);
+			}
+		}
+		return false;
+	}
+
+}

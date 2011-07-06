44da03ff485e13c9f618b26abe092a339d2eac98
Petter Holmström <petter.holmstrom@abo.fi>
2011-07-06 09:37:55 +0300

    Added stubs

---
+++ Sources/MVP4Vaadin/src/com/github/peholmst/mvp4vaadin/navigation/AbstractControllableViewComponent.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.github.peholmst.mvp4vaadin.navigation;
+
+/**
+ * @author peholmst
+ *
+ */
+public class AbstractControllableViewComponent {
+	// TODO Implement me!
+}
---
+++ Sources/MVP4Vaadin/src/com/github/peholmst/mvp4vaadin/navigation/i18n/AbstractControllableI18NViewComponent.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.github.peholmst.mvp4vaadin.navigation.i18n;
+
+/**
+ * @author peholmst
+ *
+ */
+public class AbstractControllableI18NViewComponent {
+	// TODO Implement me!
+
+}

7eaf110cc01418d65f9496b9cd0ec4df207c230d
Petter Holmström <petter.holmstrom@abo.fi>
2011-07-14 20:08:57 +0300

    Added skeleton interfaces for the new navigation API

---
+++ Sources/MVP4Vaadin/src/com/github/peholmst/mvp4vaadin/navigation/NavigationController.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2011 Petter Holmström
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.peholmst.mvp4vaadin.navigation;
+
+import java.util.List;
+
+import com.github.peholmst.mvp4vaadin.View;
+
+/**
+ * TODO Document me!
+ * 
+ * @author Petter Holmström
+ * @since 1.0
+ */
+public interface NavigationController extends java.io.Serializable {
+
+	boolean navigate(NavigationRequest request);
+
+	boolean navigateBack();
+		
+	List<View> getViewStack();
+	
+	View getCurrentView();
+	
+	View getFirstView();
+
+	boolean isEmpty();
+	
+	boolean clear();
+	
+	void addListener(NavigationControllerListener listener);
+	
+	void removeListener(NavigationControllerListener listener);
+}
